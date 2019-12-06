1cd18343c3fb70991a1d27a45bf4edde3ecba328
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2019-12-06 15:31:17 +1000

    add Rational and Complex, bump version to 1.0

--- lib/try_convert.rb
+++ lib/try_convert.rb
@@ -18,6 +18,24 @@ def Integer.try_convert obj
 end
 
 ##
+# Try to convert _obj_ into an Rational, using Kernel::Rational method.
+# Returns converted Rational or nil if _obj_ cannot be converted for
+# any reason.
+#
+def Rational.try_convert obj
+  Rational(obj) rescue nil
+end
+
+##
+# Try to convert _obj_ into an Complex, using Kernel::Complex method.
+# Returns converted Complex or nil if _obj_ cannot be converted for
+# any reason.
+#
+def Complex.try_convert obj
+  Complex(obj) rescue nil
+end
+
+##
 # Try to convert _obj_ into a Proc, using to_proc method.
 # Returns converted proc or nil if _obj_ cannot be converted for any
 # reason.
--- try_convert.gemspec
+++ try_convert.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
   s.name = 'try_convert'
-  s.version = '0.2'
-  s.date    = '2014-05-14'
+  s.version = '1.0'
+  s.date    = '2019-12-06'
   s.summary = 'A ruby gem that adds some missing try_convert methods.'
   s.description = 'Provides ::try_convert to several core classes.'
   s.authors = ['Matthew Kerwin']
   s.email   = ['matthew@kerwin.net.au']
   s.files   = ['lib/try_convert.rb']
   s.requirements = []
-  s.homepage = 'http://rubygems.org/gems/try_convert'
+  s.homepage = 'https://rubygems.org/gems/try_convert'
   s.license = 'ISC License'
 end
